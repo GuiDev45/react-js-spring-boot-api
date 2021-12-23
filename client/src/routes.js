da0cc97ecd96bcfbf9e3ccd4d48e0e8d47e7d50c
GuiDev45 <bortolettodev@gmail.com>
2021-12-23 08:26:30 -0300

    integrating the layout in the API

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,13 +5,14 @@ import Login from './pages/Login';
 import Books from './pages/Books';
 import NewBook from './pages/NewBook';
 
+
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Login}/>
                 <Route path="/books" component={Books}/>
-                <Route path="/books/new" component={NewBook}/>
+                <Route path="/book/new/:bookId" component={NewBook}/>
             </Switch>
         </BrowserRouter>
     );
